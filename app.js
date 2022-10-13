3ae609bcbd44d61096c25824b69b46d36a71032f
unknown <fredericreact@gmail.com>
2022-10-13 23:38:01 +0800

    add express.static to serve static files like css

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const request = require("request");
 const app = express()
 const port = 3000
 
+app.use(express.static("public"));
+//server need this to serve static files like css whereas bootstrap is remote
+
 app.listen(port, function () {
   console.log("listening port 3000");
 });

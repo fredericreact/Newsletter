2a38a8823a76a1270b25fa18b6736f091ce300b2
unknown <fredericreact@gmail.com>
2022-10-14 00:17:54 +0800

    setup heroku

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require("request");
-
-app.use(bodyParser.urlencoded({extended:true}));
+const https = require("https");
+require('dotenv').config();
 
 
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+
 app.use(express.static("public"));
 //server need this to serve static files like css whereas bootstrap is remote
 
-app.listen(port, function () {
-  console.log("listening port 3000");
-});
 
 app.get("/",function(req,res){
 
@@ -21,11 +23,58 @@ app.get("/",function(req,res){
   })
 
   app.post("/", function(req,res){
-    var firstName= req.body.firstName;
-    var lastName= req.body.lastName;
-    var email= req.body.email;
+    const firstName= req.body.firstName;
+    const lastName= req.body.lastName;
+    const email= req.body.email;
   
-  console.log(firstName,lastName,email);
+    const data = {
+        members: [
+          {
+            email_address: email,
+            status:"subscribed",
+            merge_fields:{
+            FNAME:firstName,
+            LNAME:lastName
+            }
+          }
+        ]
+      };
+
+      const jsonData = JSON.stringify(data);
   
+      const url = process.env.API_URL;
+
+      const options ={
+        method:"POST",
+        auth:process.env.API_TOKEN
+      }
+
+      const request = https.request(url, options, function(response){
+
+
+        if (response.statusCode === 200 ){
+            res.sendFile(__dirname+"/success.html");
+          } else {
+            res.sendFile(__dirname+"/failure.html");
+          }
+     
+
+        response.on("data",function(data){
+          console.log(JSON.parse(data));
+        })
+        })
+
+        request.write(jsonData);
+request.end();
   })
+  
+
+
+  app.post("/failure", function(req,res){
+    res.redirect("/");
+  });
+
+  app.listen(process.env.PORT||port, function () {
+    console.log("listening port 3000");
+  });
   
